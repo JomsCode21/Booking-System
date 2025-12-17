2f3e3d587b0fbb0f199fdbd4c4da40fb30f21ce9
JomsCode21 <iramuhjsolag@gmail.com>
2025-12-17 23:21:13 +0800

    updated database

--- cymae_booking_system.sql
+++ cymae_booking_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 16, 2025 at 03:53 PM
+-- Generation Time: Dec 17, 2025 at 04:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,7 +32,7 @@ CREATE TABLE `bookings` (
   `ref_number` varchar(20) NOT NULL,
   `user_id` int(11) NOT NULL,
   `cottage_name` varchar(100) NOT NULL,
-  `tour_type` enum('Day Tour','Night Stay') NOT NULL,
+  `tour_type` enum('Day Tour','Night Stay','24 Hours') NOT NULL,
   `check_in` date NOT NULL,
   `total_price` decimal(10,2) NOT NULL,
   `status` enum('Pending','Confirmed','Cancelled') DEFAULT 'Pending',
@@ -44,7 +44,8 @@ CREATE TABLE `bookings` (
 --
 
 INSERT INTO `bookings` (`id`, `ref_number`, `user_id`, `cottage_name`, `tour_type`, `check_in`, `total_price`, `status`, `created_at`) VALUES
-(1, 'BK-AAB049', 2, 'Cabana Deluxe', 'Night Stay', '2025-12-18', 3400.00, 'Pending', '2025-12-16 14:05:39');
+(1, 'BK-AAB049', 2, 'Cabana Deluxe', 'Night Stay', '2025-12-18', 3400.00, 'Cancelled', '2025-12-16 14:05:39'),
+(2, 'BK-10EB77', 4, 'Cabana Deluxe', 'Night Stay', '2025-12-19', 3400.00, 'Confirmed', '2025-12-17 00:55:56');
 
 -- --------------------------------------------------------
 
@@ -57,6 +58,7 @@ CREATE TABLE `cottages` (
   `name` varchar(100) NOT NULL,
   `price_day` decimal(10,2) NOT NULL,
   `price_night` decimal(10,2) NOT NULL,
+  `price_24h` decimal(10,2) NOT NULL DEFAULT 0.00,
   `capacity` varchar(50) NOT NULL,
   `image_url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -65,15 +67,15 @@ CREATE TABLE `cottages` (
 -- Dumping data for table `cottages`
 --
 
-INSERT INTO `cottages` (`id`, `name`, `price_day`, `price_night`, `capacity`, `image_url`) VALUES
-(1, 'Cabana Deluxe', 2700.00, 3400.00, '3 Pax (Max 6)', 'https://images.unsplash.com/photo-1566073771259-6a8506099945'),
-(2, 'Cozy Cabana', 2300.00, 3000.00, '2 Pax (Max 4)', 'https://images.unsplash.com/photo-1582719508461-905c673771fd'),
-(3, 'Nipa House (Double Deck)', 1700.00, 2000.00, 'Group', 'uploads/1765896163_bg.jpg'),
-(4, 'Nipa House (Single Bed)', 1400.00, 1700.00, 'Couple', 'https://images.unsplash.com/photo-1590523278135-25aa9690184e'),
-(5, 'Beach Front Cottage', 1200.00, 1500.00, 'Open', 'https://images.unsplash.com/photo-1590523278135-25aa9690184e'),
-(6, 'Open Cottage', 1000.00, 1200.00, 'Open', 'https://images.unsplash.com/photo-1590523278135-25aa9690184e'),
-(7, 'Canopy Tent', 500.00, 700.00, '4 Chairs', 'https://images.unsplash.com/photo-1590523278135-25aa9690184e'),
-(8, 'Camping Tent', 500.00, 500.00, '2 Pax', 'https://images.unsplash.com/photo-1590523278135-25aa9690184e');
+INSERT INTO `cottages` (`id`, `name`, `price_day`, `price_night`, `price_24h`, `capacity`, `image_url`) VALUES
+(1, 'Cabana Deluxe', 2700.00, 3400.00, 6000.00, '3 Pax (Max 6)', 'https://images.unsplash.com/photo-1566073771259-6a8506099945'),
+(2, 'Cozy Cabana', 2300.00, 3000.00, 5000.00, '2 Pax (Max 4)', 'https://images.unsplash.com/photo-1582719508461-905c673771fd'),
+(3, 'Nipa House (Double Deck)', 1700.00, 2000.00, 5000.00, 'Group', 'uploads/1765902431_nipa_doubledeck.png'),
+(4, 'Nipa House (Single Bed)', 1400.00, 1700.00, 0.00, 'Couple', 'uploads/1765902450_nipa_singlebed.png'),
+(5, 'Beach Front Cottage', 1200.00, 1500.00, 0.00, 'Open', 'uploads/1765902467_beachfrontcottage.png'),
+(6, 'Big Open Cottage', 1000.00, 1200.00, 0.00, '20 Pax', 'uploads/1765902484_bigcottage.png'),
+(7, 'Medium Open Cottage', 500.00, 700.00, 0.00, '15 Pax', 'uploads/1765902518_mediumopen.png'),
+(8, 'Small Open Cottage', 500.00, 500.00, 0.00, '10 Pax', 'uploads/1765902626_opencottage.png');
 
 -- --------------------------------------------------------
 
@@ -86,6 +88,7 @@ CREATE TABLE `users` (
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `contact_number` varchar(12) DEFAULT NULL,
   `role` enum('user','owner') DEFAULT 'user',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -94,9 +97,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`) VALUES
-(2, 'Jhumari Job Galos', 'iramuhjsolag@gmail.com', '$2y$10$AGVJazLRhChE/WptPK.zMeSKNH6erpoMIEt9RaM4vsqB3Fc94ALRm', 'user', '2025-12-16 14:04:54'),
-(3, 'Cymae Admin', 'cymaeadmin@gmail.com', '$2y$10$TReaiFbEmo.vGY8CTsMlk.5y/EHiag.rRvb9856pVkySaiSRHqIg6', 'owner', '2025-12-16 14:07:39');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `contact_number`, `role`, `created_at`) VALUES
+(2, 'Jhumari Job Galos', 'iramuhjsolag@gmail.com', '$2y$10$AGVJazLRhChE/WptPK.zMeSKNH6erpoMIEt9RaM4vsqB3Fc94ALRm', '09104171409', 'user', '2025-12-16 14:04:54'),
+(3, 'Cymae Admin', 'cymaeadmin@gmail.com', '$2y$10$TReaiFbEmo.vGY8CTsMlk.5y/EHiag.rRvb9856pVkySaiSRHqIg6', '0', 'owner', '2025-12-16 14:07:39'),
+(4, 'ruth', 'ruthadoptante698@gmail.com', '$2y$10$DAX3sottdpc.vgj5Hh/N6.DVjypyLGj.0/bN1ho/WYRUKyOWCZP8m', '0', 'user', '2025-12-17 00:54:56');
 
 --
 -- Indexes for dumped tables
@@ -130,7 +134,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `cottages`
@@ -142,7 +146,7 @@ ALTER TABLE `cottages`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
